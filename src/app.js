4be4e94b075b86b40e9c7be3ca6eac455f2bd1d7
Akshaya <chanchala9119@gmail.com>
2021-11-30 22:42:05 +0530

    console for error

--- src/app.js
+++ src/app.js
@@ -63,7 +63,10 @@ app.post('/', function (req, res) {
     //Running the function and catching the errors (if any)
     // ************************THIS IS THE CODE THAT NEEDS TO BE ADDED FOR THE NEXT LECTURE*************************
     // So the catch statement is executed when there is an error so if anything goes wrong the code in the catch code is executed. In the catch block we're sending back the failure page. This means if anything goes wrong send the faliure page
-    run().catch(e => res.render('failure'));
+    run().catch(e => {
+        console.log(e)
+        res.render('failure')
+    });
 });
 
 
